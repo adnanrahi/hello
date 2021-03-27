a7ff54a3555de80ee2c992beac1ebed121578dbc
adnanrahi <71041943+adnanrahi@users.noreply.github.com>
2021-03-27 13:26:01 +0530

    Update Hello11.sol

--- Hello11.sol
+++ Hello11.sol
@@ -1,9 +1,8 @@
 Hello,
 Please join me on Telegram or WhatsApp for Better Conversation..
 
-Telegram: @Mzn_Rahi
-WhatsApp: +917417994131
-Skype:   mznrahi786
+Telegram: @Petersmith18
+WhatsApp: +919525572767
 
 Please don't discuss on fiverr about Telegram or WhatsApp
 
